0b93cfe36e34ad83ebe9b0c1f6e48a517f2f1b60
Nurdaulet Turar <nurdaulet.turar.cz@gmail.com>
2024-11-02 11:23:38 +0100

    Add screen with a map

--- lib/second_route.dart
+++ lib/second_route.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:vut_itu/tile_providers.dart';
 
 class FirstRoute extends StatelessWidget {
   const FirstRoute({super.key});
@@ -20,19 +23,33 @@ class FirstRoute extends StatelessWidget {
   }
 }
 
-class SecondRoute extends StatelessWidget {
+class SecondRoute extends StatefulWidget {
   const SecondRoute({super.key});
 
+  @override
+  State<SecondRoute> createState() => _SecondRoute();
+}
+
+class _SecondRoute extends State<SecondRoute> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("App bar 2")),
-        body: Center(
-          child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text("Go Back")),
-        ));
+        body: Stack(
+        children: [
+          FlutterMap(
+              options: MapOptions(
+                initialCenter: const LatLng(51.5, -0.09),
+                initialZoom: 5,
+                cameraConstraint: CameraConstraint.contain(
+                  bounds: LatLngBounds(
+                    const LatLng(-90, -180),
+                    const LatLng(90, 180),
+                  ),
+                ),
+              ),
+              children: [openStreetMapTileLayer])
+      ],
+    ));
   }
 }
