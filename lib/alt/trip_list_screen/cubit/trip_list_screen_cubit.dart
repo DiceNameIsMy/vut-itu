11e90c5e3855b7695267ec2459ead73e6c4cad45
Nurdaulet Turar <nurdaulet.turar.cz@gmail.com>
2024-12-16 15:13:18 +0100

    Inline editing of trip name

--- lib/alt/trip_list_screen/cubit/trip_list_screen_cubit.dart
+++ lib/alt/trip_list_screen/cubit/trip_list_screen_cubit.dart
@@ -2,35 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vut_itu/alt/trip_list/cubit/trips_cubit.dart';
 import 'package:vut_itu/backend/business_logic/trip_model.dart';
+import 'package:vut_itu/logger.dart';
 
 part 'trip_list_screen_state.dart';
 
 class TripListScreenCubit extends Cubit<TripListScreenState> {
   final TripsCubit _tripsCubit;
 
-  TripListScreenCubit(this._tripsCubit) : super(TripListScreenInitial());
+  TripListScreenCubit(
+      this._tripsCubit, Map<int, TextEditingController> titleControllers)
+      : super(TripListScreenInitial(titleControllers: titleControllers));
 
-  factory TripListScreenCubit.fromContext(BuildContext context) {
-    return TripListScreenCubit(BlocProvider.of<TripsCubit>(context));
+  factory TripListScreenCubit.fromContext(
+    BuildContext context,
+    TripsState state,
+  ) {
+    var cubit = BlocProvider.of<TripsCubit>(context);
+    logger.w(
+        'Creating TripListScreenCubit from context with ${state.trips.length} trips');
+
+    return TripListScreenCubit(cubit, {});
   }
 
-  void addNewTripRequested() {
-    var newTrip = TripModel(userId: 1, name: '', startDate: DateTime.now());
-    var nameController = TextEditingController();
-    nameController.addListener(() {
-      nameController.value = nameController.value.copyWith(
-        text: nameController.text,
-        selection: TextSelection(
-            baseOffset: nameController.text.length,
-            extentOffset: nameController.text.length,),
-        composing: TextRange.empty,
-      );
-    });
+  Future<void> addNewTripRequested() async {
+    var newTrip =
+        TripModel(userId: 1, name: 'New trip', startDate: DateTime.now());
 
-    emit(TripListScreenAddNew(newTrip, nameController));
+    await _tripsCubit.createTrip(newTrip.name);
   }
 
   Future<void> refreshTripsList() async {
     await _tripsCubit.invalidateTrips();
+  }
+
+  Future<void> updateTripName(int tripId, String newName) async {
+    await _tripsCubit.updateTripName(tripId, newName);
   }
 }
